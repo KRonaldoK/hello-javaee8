f252149d931466b289cecf388233858d996c14ac
Kaoru R Kishimoto <kaoru.ronaldo@gmail.com>
2019-01-27 16:50:50 -0200

    Update HelloWorldResource.java

--- src/main/java/com/packtpub/javaee8/HelloWorldResource.java
+++ src/main/java/com/packtpub/javaee8/HelloWorldResource.java
@@ -6,6 +6,7 @@ package com.packtpub.javaee8;
 /* getting the remote modification III */
 /* development fix */
 /* development fix 2 */
+/* development fix 3 */
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
